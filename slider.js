5e81a28f55edd22de0a4b8ccfae5ebb52a5bdfe2
safehausdb <christopherabrummer@gmail.com>
2024-06-03 09:03:21 -0700

    Update slider.js
    Test variables ot manipulate the scale of images based on position within the carousel.

--- slider.js
+++ slider.js
@@ -19,3 +19,20 @@ const swiper = new Swiper('.swiper', {
         }
     });
 
+    const slides = document.querySelectorAll('.swiper-slide');
+
+    if (slides[0]) {
+        slides[0].style.width = '28.875rem';
+        slides[0].style.height = '37.75rem';
+    }
+
+    if (slides[1]) {
+        slides[1].style.width = '${28.875 * 0.3}rem';
+        slides[1].style.height = '${37.75 * 0.3}rem';
+    } 
+
+    if (slides[2]) {
+        slides[2].style.width = '${28.875 * 0.3 * 0.3}rem';
+        slides[2].style.height = '${37.75 * 0.3 * 0.3}rem';
+    }   
+
